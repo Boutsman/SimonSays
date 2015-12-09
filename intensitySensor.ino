25366adabcce4d1baebc6e1f950b5643a0ac5f35
Boutsman <Boutsman@gmail.com>
2015-12-09 23:20:10 +0100

    Update intensitySensor.ino

--- intensitySensor.ino
+++ intensitySensor.ino
@@ -3,12 +3,13 @@
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);
 CapacitiveSensor   cs_4_3 = CapacitiveSensor(4,3);
 CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);
-CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);
+CapacitiveSensor   cs_4_7 = CapacitiveSensor(4,7);
+CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);
 
 // value: 1  3  5  7  9  +  overlappende waarden: 2  4  6  8  //
 // dus value kan volgende waarden aannemen:                   // 9 stappen genoeg?
 // 1  2  3  4  5  6  7  8  9                                  //
-int value = 5;              // default value
+int sensitivity = 5;              // default value
 String valueBar = "(*)(*)(*)(*)(*)";  //default value
 
 void setup()                    
@@ -24,53 +25,53 @@ void loop()
     long start = millis();
     long sensor1 =  cs_4_2.capacitiveSensor(30);
     long sensor2 =  cs_4_3.capacitiveSensor(30);
-    long sensor3 =  cs_4_5.capacitiveSensor(30);;
-    long sensor4 =  cs_4_6.capacitiveSensor(30);;
-    long sensor5 = 0;
+    long sensor3 =  cs_4_5.capacitiveSensor(30);
+    long sensor4 =  cs_4_7.capacitiveSensor(30);
+    long sensor5 =  cs_4_8.capacitiveSensor(30);
     
-    if (sensor1 > 150 && sensor2 < 150) 
+    if (sensor1 > (sensor2 + 100)) 
     {
-      value = 1;
+      sensitivity = 1;
       valueBar = "(*)";
     } 
-    else if (sensor1 > 150 && sensor2 > 150)
+    else if (sensor1 > 100 && sensor2 > 100)
     {
-      value = 2;
+      sensitivity = 2;
       valueBar = "(*)(*)";
     }
-    else if (sensor1 < 150 && sensor2 > 150 && sensor3 < 150)
+    else if (sensor2 > (sensor1 + 100) && sensor2 > (sensor3 + 100))
     {
-      value = 3;
+      sensitivity = 3;
       valueBar = "(*)(*)(*)";
     }
-    else if (sensor2 > 150 && sensor3 > 150)
+    else if (sensor2 > 100 && sensor3 > 100)
     {
-      value = 4;
+      sensitivity = 4;
       valueBar = "(*)(*)(*)(*)";
     }
-    else if (sensor2 < 150 && sensor3 > 150 && sensor4 < 150)
+    else if (sensor3 > (sensor2 + 100) && sensor3 > (sensor4 + 100))
     {
-      value = 5;
+      sensitivity = 5;
       valueBar = "(*)(*)(*)(*)(*)";
     }
-    else if (sensor3 > 150 && sensor4 > 150)
+    else if (sensor3 > 100 && sensor4 > 100)
     {
-      value = 6;
+      sensitivity = 6;
       valueBar = "(*)(*)(*)(*)(*)(*)";
     }
-    else if (sensor3 < 150 && sensor4 > 150 && sensor5 < 150)
+    else if (sensor4 > (sensor3 + 100) && sensor4 > (sensor5 + 100))
     {
-      value = 7;
+      sensitivity = 7;
       valueBar = "(*)(*)(*)(*)(*)(*)(*)";
     }
-    else if (sensor4 > 150 && sensor5 > 150)
+    else if (sensor4 > 100 && sensor5 > 100)
     {
-      value = 8;
+      sensitivity = 8;
       valueBar = "(*)(*)(*)(*)(*)(*)(*)(*)";
     }
-    else if (sensor4 < 150 && sensor5 > 150)
+    else if (sensor5 > (sensor4 + 100))
     {
-      value = 9;
+      sensitivity = 9;
       valueBar = "(*)(*)(*)(*)(*)(*)(*)(*)(*)";
     }
 
@@ -92,5 +93,5 @@ void loop()
     Serial.print(valueBar);                 // print valueBar
     Serial.print("\n");
 
-    delay(50);                           // arbitrary delay to limit data to serial port 
+    delay(50);                             // arbitrary delay to limit data to serial port 
 }
